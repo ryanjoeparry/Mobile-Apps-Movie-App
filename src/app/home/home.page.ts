9c9638328b0968c02aea26aa843566d134b1839c
Ryan Parry <ryanjoeparry@hotmail.com>
2026-05-04 20:22:18 +0000

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonListHeader, IonLabel, IonSearchbar,} from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonListHeader, IonLabel, IonSearchbar,],
+})
+export class HomePage {
+  constructor() {}
+}
