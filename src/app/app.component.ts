4b94355700d69711327e60d738312b4eaa1bb894
Ryan Parry <ryanjoeparry@hotmail.com>
2026-05-08 20:07:58 +0000

    icon added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet,} from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonIcon,} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heartOutline, heart, } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [IonApp, IonRouterOutlet,],
+  imports: [IonApp, IonRouterOutlet, IonIcon,],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {addIcons({heartOutline, heart,});}
 }
